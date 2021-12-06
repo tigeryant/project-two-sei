8fa15a872a90d49e06220a3f902eb80dae43727e
John Davies <jd.other@hotmail.com>
2021-12-06 18:27:32 +0000

    work on layout, state, constants

--- src/ConvertCard.js
+++ src/ConvertCard.js
@@ -1,5 +1,30 @@
+import React from 'react'
+import { getCurrencyData } from './lib/api'
+
 function ConvertCard() {
-  // state...
+  // put state of 'currency' options here
+  // the state is determined by the value of the 'select' elements
+  // handle change (of select)
+  // handle submit
+
+  // const [currencyPair, setCurrencyPair] = React.useState({
+  //   original: null,
+  //   target: null,
+  // })
+
+  const handleChange = (e) => {
+    // setCurrencyPair({
+    //   // 
+    // })
+    console.log(e.target.name)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    await getCurrencyData()
+  }
+
 
   return (
     <section className="section">
@@ -8,33 +33,29 @@ function ConvertCard() {
           <p>convert card</p>
 
           {/* // form with input, two dropdowns, text and convert button */}
-          <form className="form">
+          <form className="form" onSubmit={handleSubmit}>
             <div className="columns">
               <div className="field column is-one-third">
                 <div className="control">
-                  <input className="input" type="text" placeholder="Amount">
+                  <input className="input" type="number" placeholder="Amount">
                   </input>
                 </div>
               </div>
-              <div className="control column is-one-third">
+              <div className="control column">
                 <div className="select">
-                  <select>
-                    <option>Bitcoin</option>
-                    <option>Ethereum</option>
-                    <option>Binance Coin</option>
-                    <option>Solana</option>
-                    <option>Cardano</option>
-                    <option>XRP</option>
-                    <option>Polkadot</option>
-                    <option>Terra</option>
-                    <option>Dogecoin</option>
-                    <option>Avalanche</option>
+                  <select
+                    name="original"
+                    onChange={handleChange}>
+                    {/* {map coin pairs} */}
                   </select>
                 </div>
               </div>
-              <div className="control column is-one-third">
+              <div className="control column">
                 <div className="select">
-                  <select>
+                  <select
+                    name="target"
+                    onChange={handleChange}>
+                      {/* {map coin pairs} */}
                     <option>Bitcoin</option>
                     <option>Ethereum</option>
                     <option>Binance Coin</option>
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,3 +1,7 @@
-import axios from 'axios'
+// import axios from 'axios'
 
-// const baseUrl = ''
+// const baseUrl = 'https://api.coinbase.com/v2/prices'
+
+// export function getCurrencyData() {
+//   return axios.get(`${baseUrl}/${originalCurrencyTicker}-USD/spot`)
+// }
