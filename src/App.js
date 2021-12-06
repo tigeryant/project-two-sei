d6cd7f2bc33dcfcbb59b4691dd1ba2bbaa3a75d8
John Davies <jd.other@hotmail.com>
2021-12-06 13:09:44 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import ConvertCard from './ConvertCard'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Convert between crypto trading pairs</h1>
+      <ConvertCard />
+    </>
+  )
+}
+
+export default App
---
+++ src/ConvertCard.js
@@ -0,0 +1,11 @@
+function ConvertCard() {
+  // state...
+
+  return (
+    <p>test</p>
+
+    // form with input, two dropdowns, text and convert button
+  )
+}
+
+export default ConvertCard
