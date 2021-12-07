5fe7a72f83ce5da495957c77167cbaf8d9ed0764
John Davies <jd.other@hotmail.com>
2021-12-07 18:26:32 +0000

    formatting chart

--- src/displays/TimeSeries.js
+++ src/displays/TimeSeries.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import { VictoryLine, VictoryChart, VictoryAxis } from 'victory'
-import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries } from 'react-vis'
+import { XYPlot, XAxis, YAxis, HorizontalGridLines, LineSeries, ChartLabel } from 'react-vis'
 import { getTimeSeriesData } from '../lib/utils'
 
 const numberOfDateTicks = 4
@@ -35,17 +35,28 @@ function TimeSeries() {
       <div className="columns is-centered">
         <div className="column is-three-quarters has-background-light">
           {chartData.length && (
-            <XYPlot
-              width={600}
-              height={300}
-            >
-              {/* <HorizontalGridLines /> */}
-              <LineSeries
-                color="red"
-                data={chartData.reverse()} />
-              <XAxis title="X" />
-              <YAxis />
-            </XYPlot>
+            <>
+              <h3>Coin 1/Coin 2</h3>
+              <figure className="is-flex is-justify-content-center">
+                <XYPlot
+                  margin={{ bottom: 75 }}
+                  width={600}
+                  height={300}
+                // title='this is a test'
+                >
+                  {/* <HorizontalGridLines /> */}
+                  <LineSeries
+                    color="blue"
+                    data={chartData.reverse()} />
+                  <XAxis
+
+                    tickFormat={x => ((new Date(x * 1000)).toLocaleDateString())}
+                    tickLabelAngle={-90}
+                  />
+                  <YAxis />
+                </XYPlot>
+              </figure>
+            </>
           )}
           {/* <VictoryChart>
             <VictoryAxis
