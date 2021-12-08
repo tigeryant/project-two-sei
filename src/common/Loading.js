c06c7f86d430f064cca846cb1ad93622e4a51d73
Duncan Browne <duncan.b.browne@gmail.com>
2021-12-08 15:44:13 +0000

    loading placeholder

---
+++ src/common/Loading.js
@@ -0,0 +1,7 @@
+function Loading () {
+  return (
+    <h4 className="has-text-centered">...</h4>
+  )
+}
+
+export default Loading
--- src/displays/TimeSeries.js
+++ src/displays/TimeSeries.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { XYPlot, XAxis, YAxis, LineSeries } from 'react-vis'
 import Error from '../common/Error'
+import Loading from '../common/Loading'
 import { getTimeSeriesData } from '../lib/utils'
 
 
 function TimeSeries({ inputData }) {
   const [chartData, setChartData] = React.useState([])
   const [isError, setIsError] = React.useState(false)
+  const isLoading = !isError && !chartData.length
 
   React.useEffect(() => {
     setIsError(false)
@@ -32,32 +34,32 @@ function TimeSeries({ inputData }) {
       <div className="columns is-centered">
         <div className="column is-three-quarters has-background-light">
           <h3 className="has-text-centered">{graphConvertAmount} {inputData.original} / {inputData.target}</h3>
-          {isError ? 
-            <Error errorDetailString="One of the currencies you have selected does not have time-series data."/> : (
-              chartData.length && (
-                <>
-                  <figure className="is-flex is-justify-content-center">
-                    {/* //todo: horizontal grid lines */}
-                    <XYPlot
-                      yDomain={[chartData.minValue * 0.85, chartData.maxValue * 1.25]}
-                      margin={{ bottom: 75, left: 75 }}
-                      width={600}
-                      height={300}
-                    >
-                      <LineSeries
-                        color="blue"
-                        data={chartData.reverse()} />
-                      <XAxis
+          {isError && <Error errorDetailString="One of the currencies you have selected does not have time-series data."/>}
+          {isLoading && <Loading />}
+          {!!chartData.length && (
+            <>
+              <figure className="is-flex is-justify-content-center">
+                {/* //todo: horizontal grid lines */}
+                <XYPlot
+                  yDomain={[chartData.minValue * 0.85, chartData.maxValue * 1.25]}
+                  margin={{ bottom: 75, left: 75 }}
+                  width={600}
+                  height={300}
+                >
+                  <LineSeries
+                    color="blue"
+                    data={chartData.reverse()} />
+                  <XAxis
 
-                        tickFormat={x => ((new Date(x * 1000)).toLocaleDateString())}
-                        tickLabelAngle={-60}
-                      />
-                      <YAxis
-                      />
-                    </XYPlot>
-                  </figure>
-                </>
-              ))}
+                    tickFormat={x => ((new Date(x * 1000)).toLocaleDateString())}
+                    tickLabelAngle={-60}
+                  />
+                  <YAxis
+                  />
+                </XYPlot>
+              </figure>
+            </>
+          )}
         </div>
       </div>
     </section>
