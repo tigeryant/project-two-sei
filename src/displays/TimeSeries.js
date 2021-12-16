a9ecb3a4f17a12496917a7866e2b264964a99e04
Duncan Browne <duncan.b.browne@gmail.com>
2021-12-16 16:38:08 +0000

    resize timeSeries at mount and resize

--- src/displays/TimeSeries.js
+++ src/displays/TimeSeries.js
@@ -4,12 +4,42 @@ import Error from '../common/Error'
 import Loading from '../common/Loading'
 import { getTimeSeriesData } from '../lib/utils'
 
+function graphHeight (width) {
+  return width * 0.3
+}
+
 
 function TimeSeries({ inputData }) {
   const [chartData, setChartData] = React.useState([])
   const [isError, setIsError] = React.useState(false)
   const isLoading = !isError && !chartData.length
+
+  const defaultGraphWidth = 600
+  const graphWidthPortionOfContainer = 1.00 // graph has padding styled in, but may need to resize for mobile
+  const [graphDimensions, setGraphDimensions] = React.useState({
+    width: defaultGraphWidth,
+    height: graphHeight(defaultGraphWidth),
+  })
   const graphContainerRef = React.useRef(null)
+  
+  React.useEffect(() => {
+    function handleResize() {
+      // console.log(graphContainerRef)
+      console.log('width', graphContainerRef.current ? graphContainerRef.current.offsetWidth : 0)
+      const newGraphWidth = graphContainerRef.current ? (
+        graphContainerRef.current.offsetWidth * graphWidthPortionOfContainer
+      ) : 600
+      setGraphDimensions({
+        width: newGraphWidth,
+        height: graphHeight(newGraphWidth),
+      })
+    }
+    
+    handleResize()
+    window.addEventListener('resize', handleResize)
+
+    return () => window.removeEventListener('resize', handleResize)
+  },[])
 
   React.useEffect(() => {
     setIsError(false)
@@ -30,15 +60,6 @@ function TimeSeries({ inputData }) {
     graphConvertAmount = inputData.amountForConversion
   }
 
-  React.useEffect(() => {
-    function handleResize() {
-      console.log(graphContainerRef)
-      console.log('width', graphContainerRef.current ? graphContainerRef.current.offsetWidth : 0)
-    }
-    handleResize()
-    window.addEventListener('resize', handleResize)
-  },[graphContainerRef])
-
   return (
 
     <section className="section">
@@ -54,8 +75,8 @@ function TimeSeries({ inputData }) {
                 <XYPlot
                   yDomain={[chartData.minValue * 0.85, chartData.maxValue * 1.25]}
                   margin={{ bottom: 75, left: 75 }}
-                  width={600}
-                  height={300}
+                  width={graphDimensions.width}
+                  height={graphDimensions.height}
                 >
                   <LineSeries
                     color="blue"
